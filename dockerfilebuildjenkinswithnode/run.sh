d46b87f6a1de6394636fcc8096de61054faa48d4
JAMES ARASIM <jaemzware@hotmail.com>
2016-03-09 16:06:27 -0800

    changed approach and working with grid instead of vnc and firefox

--- dockerfilebuildjenkinswithnode/run.sh
+++ dockerfilebuildjenkinswithnode/run.sh
@@ -1,1 +1,11 @@
-docker run -p 8080:8080 -p 3000:3000 -p 5900:5900 -v /Users/jameskarasim/Downloads/installed/repositories/dockerjenkins:/var/jenkins_home --name jenkinsfirefox -d jimarasim/jenkins:firefox20160308
+# run - run an image container
+# d - run as daemon
+# p - map hostport to container port
+#   50000 - jenkins slaves
+#   8080 - jenkins web ui
+#   3000 - stuffed animal war
+#   4444 - selenium grid
+# v - map host directory to volume container directory
+# name - name of the container
+docker run -d -p 50000:50000 -p 8080:8080 -p 3000:3000 -p 4444:4444 -v /Users/jameskarasim/Downloads/installed/repositories/dockerjenkins:/var/jenkins_home --name jenkinsfirefox jimarasim/jenkins:nodeandgrid
+
