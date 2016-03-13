b239e7789fe695d9e88dd20f023b86c15ce63c9c
JAMES ARASIM <jaemzware@hotmail.com>
2016-03-13 12:55:24 -0700

    updated to run against a volatile directory

--- dockerfilebuildjenkinswithnode/run.sh
+++ dockerfilebuildjenkinswithnode/run.sh
@@ -7,5 +7,6 @@
 #   4444 - selenium grid
 # v - map host directory to volume container directory
 # name - name of the container
-docker run -d -p 50000:50000 -p 8080:8080 -p 3000:3000 -p 4444:4444 -p 465:465 -p 587:587 -v /Users/jameskarasim/Downloads/installed/repositories/dockerjenkins:/var/jenkins_home --name jaemzmac jimarasim/jenkins:jaemzmac
+docker run -d -p 50000:50000 -p 8080:8080 -p 3000:3000 -p 4444:4444 -p 465:465 -p 587:587 -v /Users/jameskarasim/Downloads/installed/dockerjenkins_volatile/exposessnmp:/var/jenkins_home --name exposesnmp jimarasim/jenkins:exposesnmp
+
 
