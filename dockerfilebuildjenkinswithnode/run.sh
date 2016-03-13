952bb94006f305c054537bcd60e0d221b6d2e095
JAMES ARASIM <jaemzware@hotmail.com>
2016-03-12 16:59:45 -0800

    open smtp ports

--- dockerfilebuildjenkinswithnode/run.sh
+++ dockerfilebuildjenkinswithnode/run.sh
@@ -7,5 +7,5 @@
 #   4444 - selenium grid
 # v - map host directory to volume container directory
 # name - name of the container
-docker run -d -p 50000:50000 -p 8080:8080 -p 3000:3000 -p 4444:4444 -v /Users/jameskarasim/Downloads/installed/repositories/dockerjenkins:/var/jenkins_home --name jaemzmac jimarasim/jenkins:jaemzmac
+docker run -d -p 50000:50000 -p 8080:8080 -p 3000:3000 -p 4444:4444 -p 465:465 -p 587:587 -v /Users/jameskarasim/Downloads/installed/repositories/dockerjenkins:/var/jenkins_home --name jaemzmac jimarasim/jenkins:jaemzmac
 
