40b08a290328e2538a5744ac9f2f82f9114744f8
James Arasim <jaemzware@hotmail.com>
2016-07-01 14:27:14 -0700

    added script to rsync jenkins volatile directory with server

---
+++ scripts/bptrsyncjamesamacbookairtoseleniumbptme.sh
@@ -0,0 +1,3 @@
+#remove --dry-run to do this for real
+sudo rsync -av --dry-run /Users/jamesa/Downloads/installed/repositories/dockerjenkins_volatile.bitbucket.brownpapertickets jamesa@selenium.bpt.me:/home/jamesa/downloads/installed/dockerjenkins_volatile.bitbucket.brownpapertickets
+echo "remove --dry-run to do this for real"
