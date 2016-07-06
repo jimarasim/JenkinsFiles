f01ffa511e2f626fd3c93122f38ec4bc08154209
JAMES ARASIM <jaemzware@hotmail.com>
2016-07-06 10:44:51 -0700

    added more ports to the run script for jaemzware

--- scripts/jaemzwaremacbookpro.sh
+++ scripts/jaemzwaremacbookpro.sh
@@ -8,6 +8,6 @@
 #   587 - email
 # v - map host directory to volume container directory
 # name - name of the container
-docker run -d -p 465:465 -p 587:587 -p 50000:50000 -p 8080:8080 -p 4444:4444 -v /Users/jameskarasim/Downloads/installed/repositories/dockerjenkins_volatile.xcode.jaemzware:/var/jenkins_home --name jaemzware jimarasim/jenkins:bpt20160701
+docker run -d -p 3000:3000 -p 3001:3001 -p 3002:3002 -p 3003:3003 -p 3004:3004 -p 3005:3005 -p 465:465 -p 587:587 -p 50000:50000 -p 8080:8080 -p 4444:4444 -v /Users/jameskarasim/Downloads/installed/repositories/dockerjenkins_volatile.xcode.jaemzware:/var/jenkins_home --name jaemzware jimarasim/jenkins:latest
 
 
